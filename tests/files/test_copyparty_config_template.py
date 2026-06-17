386885bdd561c66174cb68f2ebabb59424a68107
holybaechu <holybaechu@proton.me>
2026-06-17 10:37:26 +0900

    feat: add copyparty shared-read users

--- tests/files/test_copyparty_config_template.py
+++ tests/files/test_copyparty_config_template.py
@@ -39,3 +39,30 @@ def test_copyparty_config_renders_only_supplied_accounts():
     assert "/srv/music" not in rendered
     assert "[/bjh_deepfake_contest]" not in rendered
     assert "/srv/bjh_deepfake_contest" not in rendered
+
+
+def test_copyparty_secret_users_get_shared_read_only_access():
+    rendered = render_copyparty_config(
+        [
+            {"name": "holybaechu", "password": "owner-password"},
+            {"name": "siregon72", "password": "guest-password"},
+            {"name": "ezmin1104", "password": "guest-password"},
+            {"name": "sieon", "password": "guest-password"},
+        ]
+    )
+
+    accounts_section = rendered.split("[global]", 1)[0]
+    shared_section = rendered.split("[/shared-readonly]", 1)[1].split("[/downloads]", 1)[0]
+    downloads_section = rendered.split("[/downloads]", 1)[1]
+
+    assert "siregon72: guest-password" in accounts_section
+    assert "ezmin1104: guest-password" in accounts_section
+    assert "sieon: guest-password" in accounts_section
+    assert "r: holybaechu, siregon72, ezmin1104, sieon" in shared_section
+    assert "A: holybaechu" in shared_section
+    assert "siregon72" not in downloads_section
+    assert "ezmin1104" not in downloads_section
+    assert "sieon" not in downloads_section
+    assert "A: holybaechu, siregon72" not in rendered
+    assert "A: holybaechu, ezmin1104" not in rendered
+    assert "A: holybaechu, sieon" not in rendered
